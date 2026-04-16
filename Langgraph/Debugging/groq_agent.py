1c33ed64f92863406faf26787a81eb89ac886ce6
Vinay Karakavalasa <vinaykarakavalasa@Vinays-MacBook-Air.local>
2026-04-16 13:40:58 +0530

    langgraph-tracing

---
+++ Langgraph/Debugging/groq_agent.py
@@ -0,0 +1,66 @@
+from typing import Annotated
+from typing_extensions import TypedDict
+from langchain_groq import ChatGroq
+from langgraph.graph import StateGraph, END, START
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode
+from langchain_core.tools import tool
+from langchain_core.messages import BaseMessage
+import os
+from dotenv import load_dotenv
+print(load_dotenv())
+
+
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+
+class State(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+llm = ChatGroq(model= "llama-3.3-70b-versatile", temperature=0.6)
+
+def make_default_graph():
+    graph_workflow=StateGraph(State)
+
+    def call_model(state):
+        return {"messages":[llm.invoke(state['messages'])]}
+    
+    graph_workflow.add_node("agent", call_model)
+    graph_workflow.add_edge("agent", END)
+    graph_workflow.add_edge(START, "agent")
+
+    agent=graph_workflow.compile()
+    return agent
+
+def make_alternative_graph():
+    """Make a tool-calling agent"""
+
+    @tool
+    def add(a: float, b: float):
+        """Adds two numbers."""
+        return a + b
+
+    tool_node = ToolNode([add])
+    model_with_tools = llm.bind_tools([add])
+    def call_model(state):
+        return {"messages": [model_with_tools.invoke(state["messages"])]}
+
+    def should_continue(state: State):
+        if state["messages"][-1].tool_calls:
+            return "tools"
+        else:
+            return END
+
+    graph_workflow = StateGraph(State)
+
+    graph_workflow.add_node("agent", call_model)
+    graph_workflow.add_node("tools", tool_node)
+    graph_workflow.add_edge("tools", "agent")
+    graph_workflow.add_edge(START, "agent")
+    graph_workflow.add_conditional_edges("agent", should_continue)
+
+    agent = graph_workflow.compile()
+    return agent
+
+agent=make_alternative_graph()
+
